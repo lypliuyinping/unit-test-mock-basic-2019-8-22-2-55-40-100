45e20d8343c8fd7ce2bf21d53773fb54d37c6c15
liuyinping <1689372042@qq.com>
2019-08-26 19:24:38 +0800

    first commit

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -3,6 +3,7 @@ package cashregister;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 import org.assertj.core.internal.bytebuddy.asm.Advice.Return;
@@ -35,9 +36,61 @@ public class CashRegisterTest {
         //when
     	cashRegister.process(purchase);
         //then
-    	verify(printer).print("the message which should be print.");
+    	verify(purchase).asString();
     }
-
+    @Test
+    public void should_return_item_name_and_price_when_as_string_given_mock_items() {
+//        //given
+//    	Item items=mock(Item.class);
+//    	Item[] items=new Item[] {
+//    			new Item("1", 1),
+//    			new Item("2",2)
+//    	};
+//    	Printer printer=new Printer();
+//    	Purchase purchase=new Purchase(items);
+//    	CashRegister cashRegister=new CashRegister(printer);
+//    	when(purchase.asString())
+//    	.thenReturn("the message which should be print.");
+//        //when
+//    	String resultString=purchase.asString(purchase);
+//        //then
+//  		assertEqu als();
+    	Item[] items=new Item[2];
+    	items[0] =mock(Item.class);
+    	items[1] =mock(Item.class);
+    	
+    	Purchase purchase=new Purchase(items);
+    	when(items[0].getName()).thenReturn("v8");
+    	when(items[0].getPrice()).thenReturn(10000.0);
+    	when(items[1].getName()).thenReturn("bose");
+    	when(items[1].getPrice()).thenReturn(10000.0);
+    	
+        //when
+    	String resultString=purchase.asString();
+        //then
+    	assertEquals("v8\t10000.0\nbose\t10000.0", resultString);
+    }
+    
+    @Test
+    public void should_return_item_name_and_price_when_as_given_mock_items() {
+        //given
+    	//Item[] =mock(Item.class);
+    	Item[] items=new Item[2];
+    	items[0] =mock(Item.class);
+    	items[1] =mock(Item.class);
+    	
+    	Purchase purchase=new Purchase(items);
+    	when(items[0].getName()).thenReturn("v8");
+    	when(items[0].getPrice()).thenReturn(10000.0);
+    	when(items[1].getName()).thenReturn("bose");
+    	when(items[1].getPrice()).thenReturn(10000.0);
+    	
+        //when
+    	purchase.asString();
+        //then
+    	verify(items[0]).getName();
+    }
+    
     @Test
     public void should_verify_with_process_call_with_mockito() {
     	//given
